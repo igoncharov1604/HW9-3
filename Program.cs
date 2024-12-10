ef359a7817797a8c81adde380220de3abe909d28
igoncharov1604 <i.goncharov@istu.edu.ua>
2024-12-10 18:30:01 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Лямбда-вираз у делегаті Predicate
+        Predicate<string> isNumber = input => double.TryParse(input, out _);
+
+        // Введення рядка
+        Console.Write("Введіть рядок: ");
+        string input = Console.ReadLine();
+
+        // Використання Predicate для перевірки
+        if (isNumber(input))
+        {
+            Console.WriteLine($"\"{input}\" є числом.");
+        }
+        else
+        {
+            Console.WriteLine($"\"{input}\" не є числом.");
+        }
+    }
+}
